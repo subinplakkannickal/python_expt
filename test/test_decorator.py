ba978f7898c5860fe1176d66a782ec7a96c55309
Subin George <subin.plakkannickal@gmail.com>
2020-08-08 01:29:47 +0530

    test_func_2 modified

--- test/test_decorator.py
+++ test/test_decorator.py
@@ -3,8 +3,11 @@ from source import decorator
 
 class TestDecorator(unittest.TestCase):
 
-    def test_func(self):
+    def test_func_1(self):
         self.assertEqual(decorator.func(4), 16)
+
+    def test_func_2(self):
+        self.assertNotEqual(decorator.func(4), 25)
 
 
 if __name__ == '__main__':
