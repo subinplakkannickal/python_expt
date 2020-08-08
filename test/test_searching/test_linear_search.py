38167a72f02edab66485060a25a710343b4c4d93
Subin George <subin.plakkannickal@gmail.com>
2020-08-09 01:48:21 +0530

    unit test added for linear search

---
+++ test/test_searching/test_linear_search.py
@@ -0,0 +1,22 @@
+import unittest
+from random import randint, choice
+
+from source.algorithm.searching.linear_searching import linear_searching
+
+class TestLinearSearch(unittest.TestCase):
+
+    def test_case_1(self):
+        input_list = sorted([randint(i, 50) for i in range(20)])
+        element = choice(input_list)
+
+        self.assertEqual(linear_searching(input_list, element), input_list.index(element) )
+
+    def test_case_2(self):
+        input_list = [1, 2, 3, 4, 5, 6, 7]
+
+        self.assertEqual(linear_searching(input_list, 10), input_list.index(element) )
+
+
+
+if __name__ == '__main__':
+    unittest.main()
