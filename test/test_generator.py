a818e1fd032d596dbf855a49d4e84c06ab618a2f
Subin George <subin.plakkannickal@gmail.com>
2020-08-08 17:46:12 +0530

    test added

--- test/test_generator.py
+++ test/test_generator.py
@@ -6,10 +6,13 @@ class TestGenerator(unittest.TestCase):
     def test_firstn(self):
         self.assertEqual(sum(generator.firstn(10)), 45)
 
+    def test_firstn_1(self):
+        self.assertNotEqual(sum(generator.firstn(10)), 0)
+
     def test_FirstN(self):
         self.assertEqual(sum(generator.FirstN(10)), 45)
     
-    def test_FirstN(self):
+    def test_FirstN_1(self):
         self.assertNotEqual(sum(generator.FirstN(10)), 0)
 
 
