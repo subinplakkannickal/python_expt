093f53a47d204eb1a5d0865f3d65165a7842ea7b
Subin George <subin.plakkannickal@gmail.com>
2020-08-09 01:05:16 +0530

    bubble sort unit test added

---
+++ test/test_sorting/test_bubble_sort.py
@@ -0,0 +1,27 @@
+import unittest
+from source.algorithm.sorting.bubble_sort import bubble_sort, recursive_bubble_sort
+
+class TestBubbleSort(unittest.TestCase):
+
+    def test_case_1(self):
+        input = [64, 82, 100, 34, 25, 12, 22, 11, 90]
+        expected_output = sorted(input)
+        self.assertEqual(bubble_sort(input), expected_output)
+
+    def test_case_2(self):
+        self.assertEqual(bubble_sort([1, 2, 3]]), [1, 2, 3])
+
+
+class TestRecursiveBubbleSort(unittest.TestCase):
+
+    def test_case_1(self):
+        input = [64, 82, 100, 34, 25, 12, 22, 11, 90]
+        expected_output = sorted(input)
+        self.assertEqual(recursive_bubble_sort(input), expected_output)
+
+    def test_case_2(self):
+        self.assertEqual(recursive_bubble_sort([1, 2, 3]]), [1, 2, 3])
+
+
+if __name__ == '__main__':
+    unittest.main()
